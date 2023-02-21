30fa4172d506e4cfe8d3283479f22b539e17df11
Jangwook Ryu <rxjw95@gmail.com>
2023-02-21 22:11:20 +0900

    hashCode 오버라이딩 하지 않음

--- src/test/java/org/example/date_230221/CollectionTest.java
+++ src/test/java/org/example/date_230221/CollectionTest.java
@@ -2,6 +2,9 @@ package org.example.date_230221;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CollectionTest {
@@ -12,6 +15,18 @@ public class CollectionTest {
         Money money2 = new Money(100);
 
         assertThat(money1.equals(money2)).isTrue();
+    }
+
+    @Test
+    void hashCodeTest() {
+        Money money1 = new Money(100);
+        Money money2 = new Money(100);
+
+        Set<Money> moneySet = new HashSet<>();
+        moneySet.add(money1);
+        moneySet.add(money2);
+
+        assertThat(moneySet.size()).isEqualTo(1);
     }
 
 }
