c5599aee0eef903c759a6913b9cc3a2657e338d7
Jangwook Ryu <rxjw95@gmail.com>
2023-02-21 21:44:34 +0900

    equals 오버라이딩 하지 않음

---
+++ src/test/java/org/example/date_230221/CollectionTest.java
@@ -0,0 +1,17 @@
+package org.example.date_230221;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CollectionTest {
+
+    @Test
+    void equalsTest() {
+        Money money1 = new Money(100);
+        Money money2 = new Money(100);
+
+        assertThat(money1.equals(money2)).isFalse();
+    }
+
+}
---
+++ src/test/java/org/example/date_230221/Money.java
@@ -0,0 +1,10 @@
+package org.example.date_230221;
+
+public class Money {
+    private final int value;
+
+    public Money(int value) {
+        this.value = value;
+    }
+
+}
